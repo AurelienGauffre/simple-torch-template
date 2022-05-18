b6e736e15f7185f387552b16c4068d36b7d318e3
gauffrea <aurelien.gauffre@gmail.com>
2022-05-18 19:58:37 +0200

    Add MTL

--- contrastive/exp2.py
+++ contrastive/exp2.py
@@ -16,14 +16,14 @@ from utils import save,load
 
 import argparse
 
-SWAV_EPOCHS = 2
-SAVE_EVERY_N_EPOCHS = 2
+SWAV_EPOCHS = 500
+SAVE_EVERY_N_EPOCHS = 100
 PROTOTYPES = 512
 RANDAUGMENT = True
 #SINKHORN =
 
 parser = argparse.ArgumentParser(description='Parser of parameters.')
-parser.add_argument('--batch_size', type=int, help='batch_size', default=128)
+parser.add_argument('--batch_size', type=int, help='batch_size', default=64)
 parser.add_argument('--epochs', type=int, help='number of epochs', default=100)
 parser.add_argument('--wandb', action='store_true', help='using wandb')
 parser.add_argument('--group', type=str, help='group name in wandb', default='Swav-LE-FT')
